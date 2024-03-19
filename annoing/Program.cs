3873641ea7ff133be98881f8c4577a678f276075
Szabina Boli <szabina.boli@skola.sala.se>
2024-03-19 13:56:04 +0100

    Add project files.

---
+++ annoing/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace annoing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testTal = { 1, 2, 3, };
+
+            // Summan
+            Console.WriteLine("Demonstration av Summan:");
+            Console.WriteLine("Summan av 1, 2, 3 är: " + Summan(testTal));
+
+            // Största talet
+            Console.WriteLine();
+            Console.WriteLine("Demonstration av Största talet:");
+            Console.WriteLine("Största talet av 1, 2, 3 är: "); // TODO
+        }
+
+        public static int Summan(int[] listOfNumbers)
+        {
+            int sum = 0;    
+            for (int i = 0; i < listOfNumbers.Length; i++)
+            {
+                sum = sum + listOfNumbers[i];   
+            }
+            return sum;
+        }
+
+        public static int biggest(int[] listOfNumbers)
+        {
+            int biggest = 0;
+
+            for (int i = 0; i < listOfNumbers.Length; i++)
+            {
+                if(i)
+            }
+            return biggest;
+        }
+    }
+
+}
