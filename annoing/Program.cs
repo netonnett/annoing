331007dd32d92816241054aa11e46bbdd87909fd
Szabina Boli <szabina.boli@skola.sala.se>
2024-03-22 12:19:03 +0100

    hello

--- annoing/Program.cs
+++ annoing/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             int[] testTal = { 1, 2, 3, };
+            string[] names = { "Anna", "Bertil", "Cesar" };
+            int[] swapTestData = { 8, 90, 6, 22 ,76,3};
+            string[] testPart1 = { "foo", "bar", "baz" };
+            string[] testPart2 = { "apa", "bpa", "cpa" };
 
             // Summan
             Console.WriteLine("Demonstration av Summan:");
@@ -13,7 +17,22 @@
             // Största talet
             Console.WriteLine();
             Console.WriteLine("Demonstration av Största talet:");
-            Console.WriteLine("Största talet av 1, 2, 3 är: "); // TODO
+            Console.WriteLine("Största talet av 1, 2, 3 är: " + biggest(testTal)); // TODO
+
+            Console.WriteLine("Demonstration av Contains:");
+            Console.WriteLine("Contains(Bertil): " + Contains(names, "Bertil")); // true
+            Console.WriteLine("Contains(David): " + Contains(names, "David")); // false
+
+            // Swap
+            Console.WriteLine("Demonstration av Swap:");
+            Console.WriteLine("Före: " + string.Join(",", swapTestData));
+            Swap(2, 5, swapTestData); // ska fungera med olika index!
+            Console.WriteLine("Efter: " + string.Join(",", swapTestData));
+
+            // Concatenate
+            Console.WriteLine("Demonstration av Concatenate:");
+            string[] concatResult = Concatenate(testPart1, testPart2);
+            Console.WriteLine("Sammansatt array: " + string.Join(",", concatResult));
         }
 
         public static int Summan(int[] listOfNumbers)
@@ -30,11 +49,63 @@
         {
             int biggest = 0;
 
-            for (int i = 0; i < listOfNumbers.Length; i++)
+            for (int i = 0; i < (listOfNumbers.Length -1); i++)
             {
 
+                if (listOfNumbers[i] > listOfNumbers[i + 1])
+                {
+                    biggest = listOfNumbers[i];
+                }
+                else
+                {
+                    biggest = listOfNumbers[i + 1];
+                }
             }
             return biggest;
+        }
+
+        public static bool Contains(string[] names, string name)
+        {
+            foreach(string s in names)
+            {
+                bool matchypatchy = s == name;
+                if(matchypatchy)
+                {
+                    return true;
+                }
+               
+            }
+
+            return false; // TODO
+        }
+
+        public static void Swap(int index1, int index2, int[] numbers)
+        {
+            int swapiesbefore = numbers[index2];
+
+            numbers[index2] = numbers[index1];
+
+            numbers[index1] = swapiesbefore;
+
+
+        }
+
+        public static string[] Concatenate(string[] part1, string[] part2)
+        {
+            string[] listtogether = new string[part1.Length + part2.Length];
+
+            for(int i = 0; i < part1.Length; i++)
+            {
+                listtogether = part1;
+            }
+            for (int i = 0; i < part2.Length; i++)
+            {
+                 
+            }
+
+
+
+            return listtogether;    
         }
     }
 
